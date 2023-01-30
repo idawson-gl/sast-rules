3f1b608d4a1813bca5b48cf2797419c06ecc1966
Julian Thome <jthome@gitlab.com>
2023-01-30 14:51:28 +0100

    minor

--- java/strings/test-ImproperUnicode.java
+++ java/strings/test-ImproperUnicode.java
@@ -47,28 +47,13 @@ public class ImproperUnicode {
         return Normalizer.normalize(s.toUpperCase(), Normalizer.Form.NFKC).equals("ADMIN");
     }
 
-    public static void stringNormalizationSuite() throws URISyntaxException {
-        String host1 = "https://www.evil.c\u2100.microsoft.com";
-        String host2 = "https://www.evil.c\u2101.microsoft.com";
-        String host3 = "https://www.evil.c\u2105.microsoft.com";
-        String host4 = "https://www.evil.c\u2106.microsoft.com";
-        String host5 = "https://www.evil.c\u210E.microsoft.com";
-        String host6 = "https://www.evil.c\u2121.microsoft.com";
-        String host7 = "https://\u2116dejs.org";
-        String host8 = "https://montrehac\u212A.ca";
-        String host9 = "https://evil.ca\uff0f.microsoft.com";
-
-        for(String h : Arrays.asList(host1,/*host2,host3,host4,host5,host6,*/ host7,host8,host9)) {
-            System.out.println("~~~~~");
-            System.out.println("Original URL : "+h);
-            System.out.println("");
-            System.out.println("Normalizer NFKC: "+Normalizer.normalize(h, Normalizer.Form.NFKC)); //RISKY!
-            System.out.println("Normalizer NFKD: "+Normalizer.normalize(h, Normalizer.Form.NFKD)); //RISKY!
-            System.out.println("Normalizer NFC: "+Normalizer.normalize(h, Normalizer.Form.NFC)); //RISKY!
-            System.out.println("Normalizer NFD: "+Normalizer.normalize(h, Normalizer.Form.NFD)); //RISKY!
-            System.out.println("URL.toASCIIString(): "+new URI(h).toASCIIString()); //RISKY!
-            System.out.println("URL.toString(): "+new URI(h).toString());
-            System.out.println("IDN.toASCII(): "+ IDN.toASCII(h)); //RISKY!
-        }
+    public static void stringNormalizationSuite(String s) throws URISyntaxException {
+       System.out.println("Normalizer NFKC: "+Normalizer.normalize(h, Normalizer.Form.NFKC)); //RISKY!
+       System.out.println("Normalizer NFKD: "+Normalizer.normalize(h, Normalizer.Form.NFKD)); //RISKY!
+       System.out.println("Normalizer NFC: "+Normalizer.normalize(h, Normalizer.Form.NFC)); //RISKY!
+       System.out.println("Normalizer NFD: "+Normalizer.normalize(h, Normalizer.Form.NFD)); //RISKY!
+       System.out.println("URL.toASCIIString(): "+new URI(h).toASCIIString()); //RISKY!
+       System.out.println("URL.toString(): "+new URI(h).toString());
+       System.out.println("IDN.toASCII(): "+ IDN.toASCII(h)); //RISKY!
     }
 }
