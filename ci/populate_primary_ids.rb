3f51d0cf215082e6465007bf28cba35d173982a8
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2023-06-05 18:24:26 +1000

    Add updated mappings

--- ci/populate_primary_ids.rb
+++ ci/populate_primary_ids.rb
@@ -1,5 +1,12 @@
 require 'yaml'
 
+# notes
+# bandit - do last
+# find_sec_bugs_scala - manually do resr
+# find_sec_bugs - manually do the rest
+# flawfinder is a mess
+
+
 Dir.glob('mappings/*.yml').each do |mapping_file|
   ruleset = File.basename(mapping_file, '.yml')
   mappings = YAML.safe_load(File.read(mapping_file))
@@ -7,8 +14,10 @@ Dir.glob('mappings/*.yml').each do |mapping_file|
 
     mapping['rules'].each_with_index do |rule, idx|
       next if rule.key? 'primary_id'
-      primary_id = "#{ruleset}.#{mapping['id']}-#{idx+1}"
+      primary_id = "#{ruleset}.#{mapping['id']}"
       primary_id = "#{ruleset}.#{mapping['id']}" if mapping['rules'].one?
+      primary_id = "#{ruleset}.#{mapping['id']}-#{idx+1}" if ['find_sec_bugs', 'gosec', 'security_code_scan'].include? ruleset
+      primary_id = primary_id.gsub(/_scala/, '') if ruleset == 'find_sec_bugs_scala'
       rule['primary_id'] = primary_id
     end
   end
@@ -20,13 +29,8 @@ Dir.glob('mappings/*.yml').each do |mapping_file|
 
   # fix the formatting
   mappings = File.read(mapping_file)
-  pp mappings
-  mappings = mappings.gsub(/: ([^"].+)/, ": \"\1\"")
+  mappings.gsub!(/: ([^"].+)$/, ': "\1"')
   File.open(mapping_file, 'w') do |file|
     file.write(mappings)
   end
-
-  # pp mappings
-
-  break
 end
