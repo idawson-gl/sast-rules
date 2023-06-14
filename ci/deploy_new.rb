43f1c638663f4b71b0eaf2845f63e0ec787714fe
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2023-06-14 12:23:00 +1000

    WIP

---
+++ ci/deploy_new.rb
@@ -0,0 +1,94 @@
+require 'yaml'
+require 'json'
+require_relative './autoformat.rb'
+
+DESTINATION_DIR = 'dist'
+MAPPINGS_PATH = 'mappings/*.yml'
+
+def main(version)
+  rule_lookup = RuleLookup.new(MAPPINGS_PATH)
+  package_builder = RulesetPackageBuilder.new(rule_lookup)
+  package_builder.run(version, MAPPINGS_PATH, DESTINATION_DIR)
+end
+
+class RuleLookup
+  def initialize(mappings_path)
+    @mappings_path = mappings_path
+  end
+
+  def find_all_rule_ids_for_path(path)
+    return rule_path_to_id[path]
+  end
+
+  def rule_path_to_id
+    return @rule_path_to_id if defined? @rule_path_to_id
+    @rule_path_to_id = {}
+    Dir.glob(@mappings_path).each do |mapping_file|
+      ruleset = File.basename(mapping_file, '.yml')
+      mappings = YAML.safe_load(File.read(mapping_file))
+
+      mappings[ruleset]['mappings'].each do |rule_mapping|
+        rule = rule_mapping['rules'].first()
+        @rule_path_to_id[rule['path']] = [] unless @rule_path_to_id.key? rule['path']
+        @rule_path_to_id[rule['path']].append(rule_mapping['id'])
+      end
+    end
+    return @rule_path_to_id
+  end
+end
+
+class RulesetPackageBuilder
+
+  def initialize(rule_lookup)
+    @rule_lookup = rule_lookup
+  end
+
+  def run(version, mappings_path, destination_path)
+    Dir.glob(mappings_path).each do |mapping_file|
+      ruleset = File.basename(mapping_file, '.yml')
+      mappings = YAML.safe_load(File.read(mapping_file))
+  
+      native_id = mappings[ruleset]['native_id']
+
+      generated_rules = []
+      mappings[ruleset]['mappings'].each do |rule_mapping|
+        mapping_rule_data = rule_mapping['rules'].first
+  
+        parsed_rule = YAML.safe_load(File.read("#{mapping_rule_data['path']}.yml"))['rules'].first()
+        parsed_rule['id'] = mapping_rule_data['id']
+        parsed_rule['metadata']['primary_identifier'] = mapping_rule_data['primary_id']
+        parsed_rule['metadata']['secondary_identifiers'] = generate_secondary_identifiers(mapping_rule_data['path'], native_id)
+        generated_rules.append(parsed_rule)
+      end
+  
+      formatted_rule = AutoFormat.reformat_yaml('', { 'rules' => generated_rules })
+      write_rule(formatted_rule, ruleset, version)
+    end
+  end
+
+  def write_rule(rule, ruleset, version)
+    File.open("#{DESTINATION_DIR}/#{ruleset}.yml", 'w') do |file|
+      file.puts('# yamllint disable')
+      file.puts("# rule-set version: #{version}")
+      file.puts('# yamllint enable')
+      file.write(rule)
+    end
+  end
+  
+  def generate_secondary_identifiers(path, native_id)
+    secondary_identifiers = []
+    rule_ids = @rule_lookup.find_all_rule_ids_for_path(path)
+    rule_ids.uniq.each do |id|
+      secondary_identifiers << {
+        'name' => native_id['name'].gsub('$ID', id),
+        'type' => native_id['type'].gsub('$ID', id),
+        'value' => native_id['value'].gsub('$ID', id)
+      }
+    end
+  
+    return secondary_identifiers
+  end
+
+end
+
+main(ARGV[0])
