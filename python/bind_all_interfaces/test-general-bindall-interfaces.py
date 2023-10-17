1e3e308ca7a1664640b85baa5c8e9c08cfd90da7
Vishwa Bhat <vbhat@gitlab.com>
2023-10-17 10:18:32 +0530

    python: bind all interfaces

--- python/bind_all_interfaces/test-general-bindall-interfaces.py
+++ python/bind_all_interfaces/test-general-bindall-interfaces.py
@@ -1,9 +1,18 @@
 # License: Apache 2.0 (c) PyCQA
 # source: https://github.com/PyCQA/bandit/blob/master/examples/binding.py
 # hash:  8eee173
-
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 31137))
 s.bind(('192.168.0.1', 8080))
+s.bind(('::', 8080))
+s.bind(('127.0.0.1', 8080), ('', 8080))
+
+unsafe = '0.0.0.0'
+s.bind((unsafe, 8080))
+
+s.bind(('127.0.0.1', 8080)) # safe
+
+safe = '192.22.23.1'
+s.bind((safe, 8080)) # safe
