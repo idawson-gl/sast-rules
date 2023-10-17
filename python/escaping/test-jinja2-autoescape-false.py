c45963628261a4a79543e501cbccbb68835e43df
Vishwa Bhat <vbhat@gitlab.com>
2023-10-17 18:05:38 +0530

    Python: Jinja 2 Autoescape

--- python/escaping/test-jinja2-autoescape-false.py
+++ python/escaping/test-jinja2-autoescape-false.py
@@ -1,7 +1,6 @@
 # License: Apache 2.0 (c) PyCQA
 # source: https://github.com/PyCQA/bandit/blob/master/examples/jinja2_templating.py
 # hash:  8eee173
-
 import jinja2
 from jinja2 import Environment, select_autoescape
 templateLoader = jinja2.FileSystemLoader( searchpath="/" )
@@ -19,11 +18,21 @@ Environment(loader=templateLoader,
 Environment(loader=templateLoader,
             load=templateLoader)
 
-Environment(loader=templateLoader, autoescape=select_autoescape())
+
+params_safe = {
+    "loader": templateLoader,
+    "autoescape": select_autoescape(),
+}
+Environment(**params_safe)
+
+
+params_unsafe = {
+    "loader": templateLoader,
+}
+Environment(**params_unsafe)
 
 Environment(loader=templateLoader,
             autoescape=select_autoescape(['html', 'htm', 'xml']))
-
 
 def fake_func():
     return 'foobar'
